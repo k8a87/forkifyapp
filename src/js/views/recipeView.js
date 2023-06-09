4dcac39f5af88266cf37a5a2dcf2c308003dc2e8
Keita Iisaka <iisak8a@gmail.com>
2023-06-09 01:16:08 +0100

    fix fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import icons from 'url:../../img/icons.svg';
-import { fracty } from 'fracty';
+import { Fraction } from 'fractional';
 import View from './view';
-console.log(fracty);
+// console.log(Fraction);
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
@@ -127,7 +127,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${
-              ing.quantity ? fracty(ing.quantity).toString() : ''
+              ing.quantity ? new Fraction(ing.quantity).toString() : ''
             }</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit}</span>
